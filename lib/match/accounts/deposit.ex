1dcc46a0b2f9bbcdd9390cd00985228fa87a34ab
Alexander Flatter <alex@flatter.io>
2023-02-08 18:20:18 +0100

    Remove dead code

--- lib/match/accounts/deposit.ex
+++ lib/match/accounts/deposit.ex
@@ -9,18 +9,11 @@ defmodule Match.Accounts.Deposit do
   end
 
   def deposit_changeset(deposit, attrs) do
-    changeset =
-      deposit
-      |> cast(attrs, [:coin_value])
-      # Yeah, it's not necessary but let's be conservative here.
-      |> validate_required([:coin_value])
-      |> validate_number(:coin_value, greater_than_or_equal_to: 0)
-      |> validate_inclusion(:coin_value, [5, 10, 20, 50, 100])
-
-    user = fetch_field!(changeset, :user)
-    put_assoc(changeset, :user, %{
-      id: user.id,
-      deposit: user.deposit + get_change(changeset, :coin_value, 0)
-    })
+    deposit
+    |> cast(attrs, [:coin_value])
+    # Yeah, it's not necessary but let's be conservative here.
+    |> validate_required([:coin_value])
+    |> validate_number(:coin_value, greater_than_or_equal_to: 0)
+    |> validate_inclusion(:coin_value, [5, 10, 20, 50, 100])
   end
 end
